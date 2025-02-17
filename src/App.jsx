ad19c7f4fa442c31454d7e2952130a6a804af390
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2025-02-17 23:34:27 +0530

    sort,filter  and search

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import {Cake,MapPin} from "lucide-react"
 function App() {
   const [searchText,setSearchText] = useState("");
   const [filterUsers,setFilterUsers] = useState(USERS);
+  const [filterCity,setFilterCity]= useState("");
+  const [filterAge,setFilterAge]= useState("");
+  const [sortOrder,setSortOrder]= useState("asc");
 
   useEffect(()=>{
      if(!searchText){
@@ -28,7 +31,46 @@ function App() {
     })
     setFilterUsers(tempFilterUsers);
   },[searchText]);
+
+  useEffect(()=>{
+    if(!filterCity && !filterAge){
+      setFilterUsers(USERS);
+      return;
+     }
+
+    const tempFilterUsers=USERS.filter((user)=>{
+      if(filterCity && 
+        user.city === filterCity &&
+        filterAge && 
+        user.age === parseInt(filterAge)){
+        return true;
+      }
+
+      if(filterAge && !filterCity && user.age=== parseInt(filterAge)){
+        return true;
+      }
+      if(filterCity && !filterAge && user.city=== filterCity){
+        return true;
+      }
+
+      return false;
+    }) ;
+    setFilterUsers(tempFilterUsers);
+  },[filterCity,filterAge]);
  
+useEffect(()=>{
+  const tempsortedUsers = filterUsers.sort((a,b)=>{
+    if (sortOrder === "asc") {
+      return a.userName.localeCompare(b.userName)
+    }
+    else{
+      return b.userName.localeCompare(a.userName)
+    }
+  })
+
+  setFilterUsers([...tempsortedUsers]);
+},[sortOrder,filterUsers]);
+
   return (
     <div>
       <h1 className="text-center text-3xl m-4 bg-slate-200 p-5">Search,Sort, Filter</h1>
@@ -37,11 +79,53 @@ function App() {
 
       <input 
       placeholder="Search" 
-      className="w-1/2 bg-slate-100 p-3 focus:outline-none my-2 rounded text-2xl" value={searchText}
-      onChange={(e) => setSearchText(e.target.value)}/>
+      className="w-2/4 bg-slate-100 p-2 mr-3 block focus:outline-none my-2 rounded text-2xl" value={searchText}
+      onChange={(e) => setSearchText(e.target.value.toLocaleLowerCase())}
+      />
+</div>      
+      {searchText ? (
+        <p className="ml-0 text-xl text-center mb-6">
+          {filterUsers.length === 0 ? "Oops!! no user found" :
+          `Found ${filterUsers.length} users for search result...`}</p>
+        ) : null
+       }
 
-      </div>
-      <div className="flex flex-wrap justify-around">
+<div className="flex justify-center mb-5">
+  <div>
+     <span>Filter By City</span>
+     <select value={filterCity}
+     onChange={(e)=>{setFilterCity(e.target.value)}}>
+      <option>All</option>{
+        USERS.map((user)=>{
+          return<option key={user.city} value={user.city}>{user.city}</option>
+        })
+      }
+     </select>
+  </div>
+  <div>
+  <span>Filter By Age</span>
+     <select value={filterAge}
+     onChange={(e)=>{setFilterAge(e.target.value)}}>
+      <option>All</option>{
+        USERS.map((user)=>{
+          return<option key={user.age} value={user.age}>{user.age}</option>
+        })
+      }
+     </select>
+  </div>
+
+  <div className="ml-10">
+  <span>Sort By Name:</span>
+     <select value={sortOrder}
+     onChange={(e)=>{setSortOrder(e.target.value)}}>
+      <option value="asc">Ascending</option>
+      <option value="dsc">Descending</option>
+     </select>
+  </div>
+
+</div>
+
+      <div className="flex flex-wrap justify-around mt-10">
         {filterUsers.map((userData,index)=>{
         const {userName, age, city, imgUrl} = userData;
 
